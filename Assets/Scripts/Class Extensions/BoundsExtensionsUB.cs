4c2caafccbcd7a4d2c3d6de46566852982963273
Uvi Vagabond <rexnudusest@gmail.com>
2018-11-29 17:52:15 +0100

    Added Bounds and Vector extentions

---
+++ Assets/Scripts/Class Extensions/BoundsExtensionsUB.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class BoundsExtensionsUB
+{
+	public static BoundsInt ConvertToBoundInt (this Bounds b)
+	{
+		
+		BoundsInt bI = new BoundsInt (b.center.ConvertToVector3Int (), b.size.ConvertToVector3Int ());
+		return bI;
+	}
+
+	public static Bounds ConvertToBound (this BoundsInt bI)
+	{
+		Bounds b = new Bounds (bI.center, bI.size);
+		return b;
+	}
+}
--- Assets/Scripts/Class Extensions/VectorExtensionsUB.cs
+++ Assets/Scripts/Class Extensions/VectorExtensionsUB.cs
@@ -25,8 +25,32 @@ public static class VectorExtensionsUB
 		v.x = (float)System.Math.Round (v.x, digits);
 		v.y = (float)System.Math.Round (v.y, digits);
 		v.z = (float)System.Math.Round (v.z, digits);
-		v.w = (float)System.Math.Round (v.w, digits); 
-
+		v.w = (float)System.Math.Round (v.w, digits);
 		return v;
 	}
+
+	public static Vector3Int ConvertToVector3Int (this Vector3 v)
+	{
+		Vector3Int vI = Vector3Int.CeilToInt (v);
+		return vI;
+	}
+
+	public static Vector3 ConvertToVector3 (this Vector3Int vI)
+	{
+		Vector3 v = new Vector3 (vI.x, vI.y, vI.z);
+		return v;
+	}
+    public static Vector2Int ConvertToVector2Int(this Vector2 v)
+    {
+        Vector2Int vI = Vector2Int.CeilToInt(v);
+        return vI;
+    }
+
+    public static Vector2 ConvertToVector2(this Vector2Int vI)
+    {
+        Vector2 v = new Vector2(vI.x, vI.y);
+        return v;
+    }
+
+
 }
